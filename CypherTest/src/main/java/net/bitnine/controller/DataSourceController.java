d7348ed60f6320878e81c2b3887ee5c4b309f8da
lionkim <cppcode@naver.com>
2017-07-25 17:13:28 +0900

    URL 변경

--- CypherTest/src/main/java/net/bitnine/controller/DataSourceController.java
+++ CypherTest/src/main/java/net/bitnine/controller/DataSourceController.java
@@ -1,17 +1,11 @@
 package net.bitnine.controller;
 
-
-
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.bitnine.domain.dto.DataSourceDTO;
@@ -19,53 +13,40 @@ import net.bitnine.service.DatabaseService;
 
 /**
  * 사용자가 전송한 정보로 dataSource를 만드는 컨트롤러
- * 세션에 dataSource를 저장, 해제 하는 부분은 interceptor에서 처리
+ * 세션에 dataSource를 저장, 해제
  * @author cppco
  *
  */
+@RequestMapping("/api/v1/db/")
 @RestController
 public class DataSourceController {
-
 	
 	public static final String DATASOURCE = "dataSource";
 	
 	@Autowired private DatabaseService databaseService;
 	
-	/*@RequestMapping("/dbConnect")
-	@ResponseBody
-	public String dbConnect(DataSourceDTO dataSourceDTO, HttpSession session, Model model)  {
-
-		DataSource dataSource = databaseService.createDataSource(dataSourceDTO);
-		System.out.println("dataSource: " + dataSource);
-		
-		model.addAttribute(DATASOURCE, dataSource);
-		
-//		return "/";
-		return "DbConnect Success";
-	}*/
-	
-	@RequestMapping("/dbConnect")
-	public String dbConnect(DataSourceDTO dataSourceDTO, HttpSession session, Model model)  {
+	@RequestMapping("/connect")
+	public String connect(DataSourceDTO dataSourceDTO, HttpSession session, Model model)  {
 
 		DataSource dataSource = databaseService.createDataSource(dataSourceDTO);
 //		System.out.println("dataSource: " + dataSource);
 		
 		if (dataSource != null) {
 			session.setAttribute(DATASOURCE, dataSource);
-			return "DbConnect Success";
+			return "Database Connect Success";
 		}
-		return "DbConnect Failed";
+		return "Database Connect Failed";
 	}
 	
-	@RequestMapping("/dbConnectOut")
-	public String findGeneral(HttpSession session, Model model)  {
+	@RequestMapping("/disconnect")
+	public String disConnect(HttpSession session, Model model)  {
 		
 		if (session.getAttribute(DATASOURCE) != null) {
 			session.removeAttribute(DATASOURCE);
-			return "DbConnectOut Success";
+			return "Database DisConnect Success";
 		}
 
-		return "DbConnectOut Failed";
+		return "Database DisConnect Failed";
 	}
 }
 
--- CypherTest/src/main/java/net/bitnine/controller/JsonObjectController.java
+++ CypherTest/src/main/java/net/bitnine/controller/JsonObjectController.java
@@ -12,14 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import net.bitnine.service.JsonObjectService;
 
+/**
+ * 사용자의 query요청을 처리하는 컨트롤러
+ * @Author  : 김형우
+ * @Date	  : 2017. 7. 25.
+ *
+ */
 @RestController
 public class JsonObjectController {
-	
+
 	@Autowired private JsonObjectService service;
 
-	@RequestMapping(value="/getJson",method=RequestMethod.POST)
+	@RequestMapping(value="/api/v1/db/query",method=RequestMethod  .POST)
 	public JSONObject getJson(String query, HttpServletRequest request) throws  IOException {
-			
+	    
 		return service.getJson(query, request);
 	}
 	
