123c55ec25da4651277be633bf9032c8e56683d6
lionkim <cppcode@naver.com>
2017-07-27 00:46:52 +0900

    JJWT 구현중

--- CypherTest/src/main/java/net/bitnine/CypherTestApplication.java
+++ CypherTest/src/main/java/net/bitnine/CypherTestApplication.java
@@ -1,10 +1,20 @@
 package net.bitnine;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class CypherTestApplication {
+
+    @RequestMapping("/")
+    String hello() {
+        return "hello world";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CypherTestApplication.class, args);
